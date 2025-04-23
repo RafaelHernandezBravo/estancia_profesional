18a4e5097daf1fc2c2781928667fa2a6f4a497ec
Rafael López Martínez <rafaellopezmartinez@Rafaels-MacBook-Air.local>
2025-04-23 15:53:12 -0600

    actualizando procesamiento de comercio al pormenor, actividades no gubernamentales y alojamiento y alimentación

--- procesando_denue_2.R
+++ procesando_denue_2.R
@@ -4,9 +4,7 @@ df = read.csv("diccionario_denue_11_2024.csv", encoding = "latin1")
 load("NOMBRE.RData")
 # Verificando si hay que unir al comercio al pormenor (4 csv), otros servicios no
 # gubernamentales (2 csv) o alojamientos y alimentación (2 csv)
-
 lista_comercio_al_pormenor = Z[4:7]
-
 for(i in seq_along(lista_comercio_al_pormenor)) {
   if(i == 1 | i == 2) {
     db_comercio_al_pormenor = merge(lista_comercio_al_pormenor[[1]], lista_comercio_al_pormenor[[2]], by = 0, all = T)
@@ -17,15 +15,38 @@ for(i in seq_along(lista_comercio_al_pormenor)) {
   db_comercio_al_pormenor$Row.names = NULL
 }
 
-db_servicios_no_gubernamentales = merge(Z[[14]], Z[[15]], by = 0, all = T)
-rownames(db_servicios_no_gubernamentales) = db_servicios_no_gubernamentales$Row.names
-db_servicios_no_gubernamentales$Row.names = NULL
+# El dataset de servicios no gubernamentales no se puede generar mediante merges o joins porque
+# tienen las mismas columnas y las filas representan a municipios distintos en su mayoría
+# sin embargo, tienen en común 113 municipios por lo que tenemos que sumar aquellos municipios
+# que son comunes a las dos tablas antes de aplicar un row bind, es decir, una unión de filas.
+mpos_no_gub14 = rownames(Z[[14]])
+mpos_no_gub15 = rownames(Z[[15]])
+mpos_comunes = intersect(mpos_no_gub14, mpos_no_gub15)
+db_servicios_no_gub_mpos_comunes = Z[[14]][mpos_comunes,] + Z[[15]][mpos_comunes,]
+db_servicios_no_gub_1 = Z[[14]][!rownames(Z[[14]]) %in% mpos_comunes,] 
+db_servicios_no_gub_2 = Z[[15]][!rownames(Z[[15]]) %in% mpos_comunes,] 
+db_servicios_no_gubernamentales = rbind(rbind(db_servicios_no_gub_1, db_servicios_no_gub_mpos_comunes),db_servicios_no_gub_2)
 
-db_servicios_alojamiento_alimentos = merge(Z[[16]], Z[[17]], by = 0, all = T)
-rownames(db_servicios_alojamiento_alimentos) = db_servicios_alojamiento_alimentos$Row.names
-db_servicios_no_gubernamentales$Row.names = NULL
+# En el caso de alojamiento y servicios de alimentación tenemos la misma situación que
+# en servicios no gubernamentales, es decir municipios repetidos en dos tablas distintas pero con
+# las mismas columnas (unidades económicas), por lo que sumamos la información de los
+#municipios comunes para así no tener municipios duplicados
+dim(Z[[16]])
+dim(Z[[17]])
+mpos_comunes_alojamiento = intersect(rownames(Z[[16]]),rownames(Z[[17]]))
+db_alojamiento_mpos_comunes = Z[[16]][mpos_comunes_alojamiento,] + Z[[17]][mpos_comunes_alojamiento,]
+db_alojamiento_1 = Z[[16]][!rownames(Z[[16]]) %in% mpos_comunes_alojamiento,] 
+db_alojamiento_2 = Z[[17]][!rownames(Z[[17]]) %in% mpos_comunes_alojamiento,] 
+db_alojamiento = rbind(rbind(db_alojamiento_1, db_alojamiento_mpos_comunes), db_alojamiento_2)
 
 Z[c(4:7,14:17)] = NULL
+
+Z[[18]] = db_comercio_al_pormenor
+names(Z)[18] = "denue_inegi_46111-46911"
+Z[[19]] = db_servicios_no_gubernamentales
+names(Z)[19] = "denue_inegi_81"
+Z[[20]] = db_alojamiento
+names(Z)[20] = "denue_inegi_72"
 # Transformar de TABLE a Data.frame.matrix, Realizar un Merge --> (Outter join) y Checar lo de Comercio al por menor para ver si es combeniente hacer un bine  
 # El 1 sum los totales de las filas y el 2 sum los totales de las columnas 
 apply(Z[[1]],1,sum)
