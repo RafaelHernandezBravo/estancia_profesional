b5727ae91d87d5e461756c677628e344fc869ef6
Soy Rafael <rafael.hernandez.bravo2020@gmail.com>
2025-05-16 13:01:24 -0600

    Subiendo script de descarga automática de shapefile

---
+++ descarga_shapefile.R
@@ -0,0 +1,31 @@
+# RUTA AL DIRECTORIO DE TRABAJO
+
+# ESTABLECER DIRECTORIO DE TRABAJO
+# ver contenido del directorio 
+df = read.csv("diccionario_denue_11_2024.csv", encoding = "latin1")
+# Creando directorio denue
+dir_path = "archivos_shp_denue_11_2024"
+if (dir.exists(dir_path)) {
+  unlink(dir_path, recursive = TRUE)
+}
+dir.create(dir_path)
+
+# LOOP MEJORADO
+options(timeout = 900)
+
+for (i in 1:nrow(df)) {
+  print(i)
+  download_url <- df$descarga_shp[i] # Asumiendo que df$descarga_csv contiene las URLs
+  file <- df$Act...Economica[i] # Asumiendo que quieres usar esto para nombres de archivos
+  print(file)
+  
+  # Limpia el nombre del archivo para evitar problemas con caracteres especiales
+  file_clean <- gsub("[^[:alnum:]_.-]", "_", file) # Reemplaza caracteres no alfanuméricos con guiones bajos
+  
+  tryCatch({
+    download.file(download_url, destfile = paste0("archivos_shp_denue_11_2024/", file_clean, ".shp"), mode = "wb")
+    print(paste("Descarga exitosa:", file_clean, ".shp"))
+  }, error = function(e) {
+    print(paste("Error al descargar", file_clean, ".shp:", e$message))
+  })
+}
